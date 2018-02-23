6354885b78565535039249e1dc8bf7732735c24e
Michael Goberling <Michael.Goberling@gmail.com>
2018-02-22 19:46:05 -0600

    First Commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
 console.log("The app is running!");
+
+const renderApp= () => 
+{
+    const template = <p>Test</p>
+    ReactDOM.render(template, appRoot);
+};
+
+const appRoot = document.getElementById('app');
+renderApp();
