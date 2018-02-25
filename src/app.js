771a6ba34354f5f92c05751da22c97e871479dfe
Michael Goberling <Michael.Goberling@gmail.com>
2018-02-24 20:18:35 -0600

    React Commit 1.2

--- src/app.js
+++ src/app.js
@@ -11,6 +11,34 @@ const home = {
     info: ['Education Stuff','Project Stuff','Work Stuff','Reference Stuff']
 };
 
+const toggleEdu = () =>
+{
+    eduToggle = !eduToggle;
+    renderHomeApp();
+
+};
+
+const toggleProj = () =>
+{
+    projToggle = !projToggle;
+    renderHomeApp();
+
+};
+
+const toggleWork = () =>
+{
+    workToggle = !workToggle;
+    renderHomeApp();
+
+};
+
+const toggleRef = () =>
+{   
+    refToggle = !refToggle;
+    renderHomeApp();
+
+};
+
 const renderHomeApp = () =>
 {
     const homeTemplate =
@@ -18,13 +46,13 @@ const renderHomeApp = () =>
         <div>
             <h1>{home.title}</h1>
             <h2>{home.subtitle}</h2>
-            <button onClick = {eduToggle = !eduToggle}>{eduToggle ? 'Show Education' : 'Hide Education'}</button>
+            <button onClick = {toggleEdu}>{eduToggle ? 'Show Education' : 'Hide Education'}</button>
             <p>{eduToggle ? false : home.info[0]}</p>
-            <button onClick = {projToggle = !projToggle}>{projToggle ? 'Show Projects' : 'Hide Projects'}</button>
+            <button onClick = {toggleProj}>{projToggle ? 'Show Projects' : 'Hide Projects'}</button>
             <p>{projToggle ? false : home.info[1]}</p>    
-            <button onClick = {workToggle = !workToggle}>{workToggle ? 'Show Work Experience' : 'Hide Work Experience'}</button>
+            <button onClick = {toggleWork}>{workToggle ? 'Show Work Experience' : 'Hide Work Experience'}</button>
             <p>{workToggle ? false : home.info[2]}</p>
-            <button onClick = {refToggle = !refToggle}>{refToggle ? 'Show References' : 'Hide References'}</button>
+            <button onClick = {toggleRef}}>{refToggle ? 'Show References' : 'Hide References'}</button>
             <p>{refToggle ? false : home.info[3]}</p>
             <p>Test</p>
         </div>
