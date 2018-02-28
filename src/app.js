30bfe738063a67f33866cd67196792fac0f5484d
Michael Goberling <Michael.Goberling@gmail.com>
2018-02-27 22:28:54 -0600

    React Commit 4.5

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ import ResumeApp from './ResumeApp';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
-console.log("React Commit 4.4");
+console.log("React Commit 4.5");
 
 ReactDOM.render(<ResumeApp />, document.getElementById('app'));
