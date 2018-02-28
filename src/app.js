2d7e4bc964eaa8ed0fee28878e4515fc3aec0ea3
Michael Goberling <Michael.Goberling@gmail.com>
2018-02-28 03:54:12 -0600

    React Commit 6.3

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ import ResumeApp from './ResumeApp';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
-console.log("React Commit 6.2");
+console.log("React Commit 6.3");
 
 ReactDOM.render(<ResumeApp />, document.getElementById('app'));
