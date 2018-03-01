7e19cfd30510454b3d20bde2ca99927074f70a37
Michael Goberling <Michael.Goberling@gmail.com>
2018-02-28 20:39:06 -0600

    React Commit 8.9

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ import ResumeApp from './ResumeApp';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
-console.log("React Commit 8.8");
+console.log("React Commit 8.9");
 
 ReactDOM.render(<ResumeApp />, document.getElementById('app'));
